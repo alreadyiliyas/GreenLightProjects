5ae0ff7d1008bdb929527583f48ef245fa25d02e
alreadyiliyas <lukenov.iliyasl2003l@gmail.com>
2024-09-24 00:16:27 +0500

    Chapter 12.2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,9 +5,7 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"log/slog"
-	"net/http"
 	"os"
 	"time"
 
@@ -19,11 +17,16 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
-	db struct{
-		dsn string
+	db   struct {
+		dsn          string
 		maxOpenConns int
 		maxIdleConns int
 		maxIdleTime  time.Duration
+	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
 	}
 }
 
@@ -35,19 +38,23 @@ type application struct {
 
 func main() {
 	var cfg config
-	
+
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	
+
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("MOVIEDB_DB_DSN"), "PostgreSQL DSN")
 
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")	
+
 	flag.Parse()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	
+
 	db, err := openDB(cfg)
 	if err != nil {
 		logger.Error(err.Error())
@@ -63,24 +70,15 @@ func main() {
 		logger: logger,
 		models: data.NewModels(db),
 	}
-	
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-	
-	err = srv.ListenAndServe()
-	logger.Error(err.Error())
-	os.Exit(1)
 }
 
-
-func openDB(cfg config) (*sql.DB, error){
+func openDB(cfg config) (*sql.DB, error) {
 	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
 		return nil, err
@@ -91,12 +89,12 @@ func openDB(cfg config) (*sql.DB, error){
 	db.SetMaxIdleConns(cfg.db.maxIdleConns)
 
 	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
-	
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	err = db.PingContext(ctx)
-	if err != nil{
+	if err != nil {
 		db.Close()
 		return nil, err
 	}
--- go.mod
+++ go.mod
@@ -5,3 +5,8 @@ go 1.22
 require github.com/julienschmidt/httprouter v1.3.0
 
 require github.com/lib/pq v1.10.9
+
+require (
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+)
