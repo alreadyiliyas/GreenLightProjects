e148b313598194f2c0134663e92fa8fc3cf03821
alreadyiliyas <lukenov.iliyasl2003l@gmail.com>
2024-10-04 00:04:59 +0500

    Chapter 13.3.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"GoCourse/internal/data"
 	"context"
 	"database/sql"
 	"flag"
 	"log/slog"
 	"os"
 	"time"
+
+	"GoCourse/internal/data"
+	"GoCourse/internal/mailer"
 
 	_ "github.com/lib/pq"
 )
@@ -28,12 +30,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -50,9 +60,16 @@ func main() {
 
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
-	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")	
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "cec43360461fb2", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "4f967a077c105a", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "GoCourse <no-reply@greenlight.alexedwards.net>", "SMTP sender")
 
 	flag.Parse()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	db, err := openDB(cfg)
@@ -69,6 +86,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
