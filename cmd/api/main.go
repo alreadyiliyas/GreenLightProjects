68ced66df362a733fece6ad6c940ac7d395a0f39
alreadyiliyas <lukenov.iliyasl2003l@gmail.com>
2024-11-17 22:33:59 +0500

    final chapter 18

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -69,6 +73,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "cec43360461fb2", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "4f967a077c105a", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "GoCourse <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
